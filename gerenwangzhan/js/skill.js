5ed4ce26cca316a09c335e15b03bd035a1a522dc
wangxuefeng18845893757 <1517926832@qq.com>
2018-04-17 19:05:19 +0800

    个人网站(第2版本)-首页加入跳转，技能展示页面

---
+++ gerenwangzhan/js/skill.js
@@ -0,0 +1,7 @@
+$(aGrow[1]).addClass('active');
+// var ht=$(document).height();
+// $('.sanyaosu').height(ht-50);
+$('.fuhao1').css({left:'0px',opacity:1});
+$('.fuhao2').css({left:'0px',opacity:1});
+$('.tool').css({right:'0px',opacity:1});
+$('.method').css({right:'0px',opacity:1});
